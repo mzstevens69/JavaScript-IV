5e3d07a89fb59f48aeb9368d7492666b4883a79c
Michael Stevens <codemasterflash@Michaels-MacBook-Pro.local>
2019-10-17 15:55:33 -0500

    added new students and instructors and TL

--- assignments/lambda-classes.js
+++ assignments/lambda-classes.js
@@ -31,15 +31,21 @@ class Instructor extends Person {
     grade(student, subject) {
         return (`${student.name} receives a perfect score on ${subject}`);
     }//closes grade
+    stdsGrade(student) {
+        let addSubtract = Math.Random();
+
+    }
 }//closes Instructor class
 //////student class/////////////
-const favSubjects = ['JavaScript', 'Python', 'Html', 'Less' ];
+
+
 class Student extends Person {
     constructor(stdsAttrs) {
         super(stdsAttrs);
         this.previousBackground = stdsAttrs.previousBackground,
         this.className = stdsAttrs.className,
-        this.favSubjects = stdsAttrs.favSubjects
+        this.favSubjects = stdsAttrs.favSubjects,
+        this.grade = stdsAttrs.grade = 95
     }//closes constructor
     listSubjects() {
         this.favSubjects.map(subj => console.log(subj));
@@ -49,7 +55,6 @@ class Student extends Person {
     }
     sprintChallenge() {
         return (`${student.name} has begun sprint challenge on ${subject}`);
-
     }
 }//closes student
 class TeamLeader extends Instructor {
@@ -65,4 +70,55 @@ class TeamLeader extends Instructor {
         return `${this.name} debugs ${student.name}'s code on ${subject}`;
     }
 }
+const lola = new Student ({
+    name: 'Lola Stevens',
+    location: 'Portland, TN',
+    age: 21,
+    gender: 'F',
+    previousBackground: 'Dancer',
+    className: 'FT23',
+    favSubjects: ['HTML', 'Less', 'Javascript'],
+    grade: 75
+
+});
+const brit = new Instructor({
+    name: 'Brit',
+    location: 'Zoom Meeting Client',
+    age: 31,
+    gender: 'F',
+    specialty: 'instructing',
+    favLanguage: ['html', 'Javascript', 'nodejs' ],
+    catchPhrase: 'whoops'
+});
+const morty = new Student ({
+    name: 'Morty Smith',
+    location: 'Portland, TN',
+    age: 14,
+    gender: 'M',
+    previousBackground: 'Student',
+    className: 'FT1',
+    favSubjects: ['C++', 'C#', 'Python'],
+    grade: 80
+
+});
+const rick = new Instructor({
+    name: 'Rick Sanchez',
+    location: 'Zoom Meeting Client',
+    age: 'Ageless',
+    gender: 'M',
+    specialty: 'Science',
+    favLanguage: ['html', 'Javascript', 'nodejs' ],
+    catchPhrase: 'wubbalubbadubdub'
+});
+const geralt = new TeamLeader({
+    name: 'Phoenix Person',
+    location: 'Bird World',
+    age: 100,
+    gender: 'male',
+    specialty: 'being a bird',
+    favLanguage: 'Birdish',
+    catchPhrase: 'None',
+    gradClassName: 'FSW3',
+    favInstructor: 'Britt'
+});
  console.log()
